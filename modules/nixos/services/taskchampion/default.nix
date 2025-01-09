87ac12e4c608419f229fdfa16efeb6236886fa40
Justin Martin <jus10mar10@gmail.com>
2025-01-08 20:39:52 -0600

    update taskwarrior and taskchampion

--- modules/nixos/services/taskchampion/default.nix
+++ modules/nixos/services/taskchampion/default.nix
@@ -5,6 +5,8 @@
   options,
   ...
 }:
+with lib;
+with lib.frgd;
 
 let
   cfg = config.frgd.services.taskchampion;
@@ -47,16 +49,5 @@ in
       };
     };
 
-    services.caddy = {
-      enable = true;
-      virtualHosts = {
-        "tasks.fluffy-rooster.ts.net" = {
-          extraConfig = ''
-            reverse_proxy http://127.0.0.1:8084
-            encode gzip
-          '';
-        };
-      };
-    };
   };
 }
--- systems/x86_64-linux/tasks/default.nix
+++ systems/x86_64-linux/tasks/default.nix
@@ -16,4 +16,15 @@ with lib.frgd;
       };
     };
   };
+  services.caddy = {
+    enable = true;
+    virtualHosts = {
+      "tasks.fluffy-rooster.ts.net" = {
+        extraConfig = ''
+          reverse_proxy http://127.0.0.1:10222
+          encode gzip
+        '';
+      };
+    };
+  };
 }
