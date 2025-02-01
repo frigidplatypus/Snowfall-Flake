468c6a1d2abcd9efaf57b2eae124b21e24edc2bf
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-01-31 22:06:56 -0600

    add my neovim obsidian package

--- modules/home/apps/obsidian/default.nix
+++ modules/home/apps/obsidian/default.nix
@@ -1,15 +1,27 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 with lib.frgd;
-let cfg = config.frgd.apps.obsidian;
-in {
+let
+  cfg = config.frgd.apps.obsidian;
+in
+{
   options.frgd.apps.obsidian = with types; {
     enable = mkBoolOpt false "Whether or not to enable obsidian.";
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [ obsidian ];
-    nixpkgs.config.permittedInsecurePackages = [ "electron-24.8.6" ];
+    home.packages = with pkgs; [
+      obsidian
+      pandoc
+      frgd-notes.notes
+    ];
+
+    # nixpkgs.config.permittedInsecurePackages = [ "electron-24.8.6" ];
   };
 }
