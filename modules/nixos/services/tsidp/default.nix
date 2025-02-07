7ca122c3d7918a55bc67bfea330b7759405ee84b
Justin Martin <jus10mar10@gmail.com>
2025-02-07 15:36:20 -0600

    add tsidp service

---
+++ modules/nixos/services/tsidp/default.nix
@@ -0,0 +1,48 @@
+{
+  options,
+  config,
+  lib,
+  ...
+}:
+
+with lib;
+with lib.frgd;
+let
+  cfg = config.frgd.services.tsidp;
+in
+{
+  options.frgd.services.tsidp = with types; {
+    enable = mkBoolOpt false "tsidp";
+    dataDir = mkOption {
+      type = types.path;
+      default = "/var/lib/tsidp";
+      description = "tsidp state dir";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    systemd.services.tsidp = {
+      enable = true;
+      description = "Tailscale OpenID Connect service";
+      wants = [ "network-online.target" ];
+      after = [ "network-online.target" ];
+      wantedBy = [ "multi-user.target" ];
+
+      environment = {
+        TS_HOSTNAME = "idp";
+        TS_USERSPACE = "false";
+        TAILSCALE_USE_WIP_CODE = "1";
+        TS_STATE_DIR = "${cfg.dataDir}";
+      };
+
+      serviceConfig = {
+        Type = "simple";
+        RestartSec = 5;
+        Restart = "always";
+        User = "root";
+        ExecStart = "${pkgs-unstable.tailscale}/bin/tsidp -use-local-tailscaled";
+      };
+    };
+
+  };
+}
