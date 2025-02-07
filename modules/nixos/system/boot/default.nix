d8433919a64234b317a58e8c75137da9afae67e2
Justin Martin <jus10mar10@gmail.com>
2025-02-06 21:16:41 -0600

    change efi boot option

--- modules/nixos/system/boot/default.nix
+++ modules/nixos/system/boot/default.nix
@@ -1,8 +1,16 @@
-{ options, config, pkgs, lib, ... }:
+{
+  options,
+  config,
+  pkgs,
+  lib,
+  ...
+}:
 with lib;
 with lib.frgd;
-let cfg = config.frgd.system.boot;
-in {
+let
+  cfg = config.frgd.system.boot;
+in
+{
   options.frgd.system.boot = with types; {
     enable = mkBoolOpt false "Whether or not to enable booting.";
     grub = mkBoolOpt false "Enable grub";
@@ -22,7 +30,6 @@ in {
     };
     boot.loader.efi = mkIf cfg.efi {
       canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot/efi";
     };
   };
 }
