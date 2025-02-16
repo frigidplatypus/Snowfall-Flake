314ad21f597f3e73fa6a199e48dd49ae1a3c348b
Justin Martin <jus10mar10@gmail.com>
2025-02-15 20:34:10 -0600

    update cliflux

--- packages/cliflux/default.nix
+++ packages/cliflux/default.nix
@@ -10,17 +10,18 @@ let
     name = "cliflux";
     owner = "spencerwi";
     repo = "cliflux";
-    rev = "v1.4.4";
+    rev = "v1.5.0";
     sha256 = "sha256-plfJnKdKsPH2VCexOpO0jduF5cXD4cGHi+eviuueaMY=";
   };
 in
 
 pkgs.rustPlatform.buildRustPackage {
   pname = "cliflux";
-  version = "v1.4.4";
+  version = "v1.5.0";
   src = repo;
-  cargoHash = "sha256-TtK1hN1RNusVWxWVTity+N1cpfauhfeQpkqAOI1fIco=";
+  cargoHash = "sha256-tTLsd/KavSpGXyGRbpVGQiY7tnDfkGz5N/9zni2fvJA=";
   PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
+    useFetchCargoVendor = true;
   OPENSSL_NO_VENDOR = 1;
   nativeBuildInputs = with pkgs; [
     pkg-config
