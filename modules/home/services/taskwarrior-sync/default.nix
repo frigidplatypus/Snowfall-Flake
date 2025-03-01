4d2298e33a726785da0079078c8402adbb11792b
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-02-28 19:22:26 -0600

    add personal taskwarrior-sync

--- modules/home/services/taskwarrior-sync/default.nix
+++ modules/home/services/taskwarrior-sync/default.nix
@@ -1,13 +1,47 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.frgd;
 
 let
-  inherit (lib) mkEnableOption mkIf;
 
   cfg = config.frgd.services.taskwarrior-sync;
-in {
-  options.frgd.services.taskwarrior-sync = {
+in
+{
+  options.frgd.services.taskwarrior-sync = with types; {
     enable = mkEnableOption "Enable Taskwarrior Sync service";
+    frequency = mkOpt str "*:0/5" "How often to run `taskwarrior sync`.";
+    pkg = mkOpt package pkgs.taskwarrior3 "The version of Taskwarrior to use ";
   };
 
-  config = mkIf cfg.enable { services.taskwarrior-sync.enable = true; };
+  config = mkIf cfg.enable {
+
+    systemd.user.services.taskwarrior-sync = {
+      Unit = {
+        Description = "Taskwarrior sync";
+      };
+      Service = {
+        CPUSchedulingPolicy = "idle";
+        IOSchedulingClass = "idle";
+        ExecStart = "${cfg.pkg}/bin/task synchronize";
+      };
+    };
+
+    systemd.user.timers.taskwarrior-sync = {
+      Unit = {
+        Description = "Taskwarrior periodic sync";
+      };
+      Timer = {
+        Unit = "taskwarrior-sync.service";
+        OnCalendar = cfg.frequency;
+      };
+      Install = {
+        WantedBy = [ "timers.target" ];
+      };
+    };
+  };
 }
