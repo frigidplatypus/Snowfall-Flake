387753ef8c53e6035d1ff42bcf5bd48f87641255
Justin Martin <jus10mar10@gmail.com>
2025-06-02 21:29:50 -0500

    reformat

--- modules/home/desktop/addons/mako/default.nix
+++ modules/home/desktop/addons/mako/default.nix
@@ -1,21 +1,33 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 with lib;
 with lib.frgd;
 let
   inherit (lib) mkEnableOption mkIf;
 
   cfg = config.frgd.desktop.addons.mako;
-in {
-  options.frgd.desktop.addons.mako = { enable = mkEnableOption "mako"; };
+in
+{
+  options.frgd.desktop.addons.mako = {
+    enable = mkEnableOption "mako";
+  };
 
   config = mkIf cfg.enable {
 
-    home = { packages = with pkgs; [ libnotify ]; };
+    home = {
+      packages = with pkgs; [ libnotify ];
+    };
     services.mako = {
       enable = true;
-      backgroundColor = "#${colorScheme.palette.base09}";
-      borderColor = "#${colorScheme.palette.base0F}";
-      defaultTimeout = 15000;
+      settings = {
+        backgroundColor = "#${colorScheme.palette.base09}";
+        borderColor = "#${colorScheme.palette.base0F}";
+        defaultTimeout = 15000;
+      };
     };
   };
 }
