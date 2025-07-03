736bd8ec7e6ffad391efac516254b79d4515521d
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-07-03 08:11:35 -0500

    enable vikunja, disable taskwarrior server

--- systems/x86_64-linux/tasks/default.nix
+++ systems/x86_64-linux/tasks/default.nix
@@ -10,18 +10,30 @@ with lib.frgd;
 
   frgd = {
     archetypes.lxc = enabled;
-    services = {
-      taskchampion = {
-        enable = true;
-      };
-    };
+    # services = {
+    #   taskchampion = {
+    #     enable = true;
+    #   };
+    # };
+  };
+  services.vikunja = {
+    enable = true;
+    frontendScheme = "https";
+    frontendHostname = "tasks.fluffy-rooster.ts.net";
+
   };
   services.caddy = {
     enable = true;
     virtualHosts = {
+      # "tasks.fluffy-rooster.ts.net:10222" = {
+      #   extraConfig = ''
+      #     reverse_proxy http://127.0.0.1:10222
+      #     encode gzip
+      #   '';
+      # };
       "tasks.fluffy-rooster.ts.net" = {
         extraConfig = ''
-          reverse_proxy http://127.0.0.1:10222
+          reverse_proxy http://127.0.0.1:3456
           encode gzip
         '';
       };
