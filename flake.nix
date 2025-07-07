1ec7e77084255961ca1f973cf2f6a46d2671eda3
Justin Martin <jus10mar10@gmail.com>
2025-07-07 07:23:00 -0500

    update stable nixpkgs

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
   inputs = {
 
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    stable-nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
+    stable-nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
 
     home-manager = {
       url = "github:nix-community/home-manager";
