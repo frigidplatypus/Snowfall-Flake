bb8543b5e45555037f23479e2be01a86445ec8d2
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-08-29 05:42:30 -0500

    reenable taskchamp

--- systems/x86_64-linux/tasks/default.nix
+++ systems/x86_64-linux/tasks/default.nix
@@ -11,28 +11,28 @@ with lib.frgd;
   frgd = {
     archetypes.lxc = enabled;
     virtualization.docker = enabled;
-    # services = {
-    #   taskchampion = {
-    #     enable = true;
-    #   };
-    # };
+    services = {
+      taskchampion = {
+        enable = true;
+      };
+    };
   };
   services.vikunja = {
     enable = true;
     frontendScheme = "https";
-    frontendHostname = "tasks.fluffy-rooster.ts.net";
+    frontendHostname = "tasks.fluffy-rooster.ts.net:8000";
 
   };
   services.caddy = {
     enable = true;
     virtualHosts = {
-      # "tasks.fluffy-rooster.ts.net:10222" = {
-      #   extraConfig = ''
-      #     reverse_proxy http://127.0.0.1:10222
-      #     encode gzip
-      #   '';
-      # };
       "tasks.${tailnet}" = {
+        extraConfig = ''
+          reverse_proxy http://127.0.0.1:10222
+          encode gzip
+        '';
+      };
+      "tasks.${tailnet}:8000" = {
         extraConfig = ''
           reverse_proxy http://127.0.0.1:3456
           encode gzip
