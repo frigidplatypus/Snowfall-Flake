aa1b4504c08b42a91811a5c48245ccbb705249e4
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-08-30 12:36:33 -0500

    add hm task-herald module

--- homes/x86_64-linux/justin@tasks/default.nix
+++ homes/x86_64-linux/justin@tasks/default.nix
@@ -1,5 +1,6 @@
 {
   lib,
+  config,
   # pkgs,
   ...
 }:
@@ -8,6 +9,7 @@ with lib.frgd;
 {
   # home.packages = with pkgs; [ frgd.taskwarrior-api ];
   frgd = {
+    security.sops = enabled;
     user = {
       enable = true;
       name = "justin";
@@ -24,5 +26,30 @@ with lib.frgd;
       ranger = enabled;
     };
 
+  };
+  sops.secrets.task_herald_ntfy = { };
+
+  services.task-herald = {
+    enable = true;
+    settings = {
+      # Required: notification service URL
+
+      shoutrrr_url_file = config.sops.secrets.task_herald_ntfy.path;
+
+      # Optional settings with defaults
+      poll_interval = "30s";
+      sync_interval = "30s";
+      log_level = "info";
+
+      # Web interface settings
+      web = {
+        host = "100.108.249.12";
+        port = 8080;
+        auth = false;
+      };
+
+      # Custom notification message template
+      notification_message = "🔔 {{.Description}} (Due: {{.Due}})";
+    };
   };
 }
