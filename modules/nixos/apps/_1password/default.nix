c0ddb7eaf05b4c019d6fbcf355992b94a7cc9dd9
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-10-04 08:50:06 -0500

    reformat

--- modules/nixos/apps/_1password/default.nix
+++ modules/nixos/apps/_1password/default.nix
@@ -1,9 +1,17 @@
-{ options, config, lib, pkgs, ... }:
+{
+  options,
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 with lib;
 with lib.frgd;
-let cfg = config.frgd.apps._1password;
-in {
+let
+  cfg = config.frgd.apps._1password;
+in
+{
   options.frgd.apps._1password = with types; {
     enable = mkBoolOpt false "Whether or not to enable 1password.";
   };
