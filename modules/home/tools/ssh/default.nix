9f159cbe3e9211f2ad8e5b0365155d1a69c12fb9
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-10-04 08:53:56 -0500

    update ssh config for 25.11

--- modules/home/tools/ssh/default.nix
+++ modules/home/tools/ssh/default.nix
@@ -13,17 +13,22 @@ in
     services.ssh-agent = enabled;
     programs.ssh = {
       enable = true;
-      addKeysToAgent = "yes";
+      # Disable default config to avoid deprecation warnings
+      enableDefaultConfig = false;
       matchBlocks = {
+        # Default configuration for all hosts
+        "*" = {
+          addKeysToAgent = "yes";
+          # Other common default settings can be added here
+        };
+        
+        # Specific host configurations
         soft = {
           port = 23231;
           hostname = "git.frgd.us";
         };
       };
-      extraConfig = ''
-        Host *
-          IdentityAgent ~/.1password/agent.sock
-      '';
+      # IdentityAgent configuration moved to frgd.apps._1password module
     };
   };
 }
