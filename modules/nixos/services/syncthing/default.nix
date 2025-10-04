da85057131a883e54135730713a9784a511b8f5d
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-10-04 08:50:28 -0500

    reformat

--- modules/nixos/services/syncthing/default.nix
+++ modules/nixos/services/syncthing/default.nix
@@ -1,24 +1,29 @@
 # TODO Fix user settings to pull from configuration instead of being hardcoded.
 
-{ options, config, lib, ... }:
+{
+  options,
+  config,
+  lib,
+  ...
+}:
 
 with lib;
 with lib.frgd;
 let
   cfg = config.frgd.services.syncthing;
-in {
+in
+{
   options.frgd.services.syncthing = with types; {
     enable = mkBoolOpt false "Whether or not to enable Syncthing.";
+  };
 
   config = mkIf cfg.enable {
     services = {
       syncthing = {
         enable = true;
         user = "justin";
-        dataDir =
-          "/home/justin/syncthing"; # Default folder for new synced folders
-        configDir =
-          "/home/justin/.config/syncthing"; # Folder for Syncthing's settings and keys
+        dataDir = "/home/justin/syncthing"; # Default folder for new synced folders
+        configDir = "/home/justin/.config/syncthing"; # Folder for Syncthing's settings and keys
         guiAddress = "0.0.0.0:8384";
       };
     };
