e8595d67db5612311c44e5cd269a2d4bc0ba0622
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-10-12 21:45:56 -0500

    cleanup tasks

--- systems/x86_64-linux/tasks/default.nix
+++ systems/x86_64-linux/tasks/default.nix
@@ -12,15 +12,9 @@ with lib.frgd;
     (modulesPath + "/virtualisation/proxmox-lxc.nix")
   ];
 
-  environment.systemPackages = with pkgs; [
-    devenv
-    direnv
-  ];
-
   # Enable networking
   frgd = {
     archetypes.lxc = enabled;
-    virtualization.docker = enabled;
     cli-apps.tmux = enabled;
     security.sops = enabled;
     services = {
@@ -29,11 +23,6 @@ with lib.frgd;
       };
     };
   };
-  services.vikunja = {
-    enable = true;
-    frontendScheme = "https";
-    frontendHostname = "tasks.fluffy-rooster.ts.net:8000";
-  };
 
   services.caddy = {
     enable = true;
@@ -41,12 +30,6 @@ with lib.frgd;
       "tasks.${tailnet}" = {
         extraConfig = ''
           reverse_proxy http://127.0.0.1:10222
-          encode gzip
-        '';
-      };
-      "tasks.${tailnet}:8000" = {
-        extraConfig = ''
-          reverse_proxy http://127.0.0.1:3456
           encode gzip
         '';
       };
