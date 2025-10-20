3ce97ab4e94df966851c7e0e84303417b9e8dba2
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-10-20 16:34:52 -0500

    add flakeRoot

---
+++ lib/module/default.nix
@@ -0,0 +1,79 @@
+{ lib, ... }:
+
+with lib;
+rec {
+  ## Create a NixOS module option.
+  ##
+  ## ```nix
+  ## lib.mkOpt nixpkgs.lib.types.str "My default" "Description of my option."
+  ## ```
+  ##
+  #@ Type -> Any -> String
+  mkOpt =
+    type: default: description:
+    mkOption { inherit type default description; };
+
+  ## Create a NixOS module option without a description.
+  ##
+  ## ```nix
+  ## lib.mkOpt' nixpkgs.lib.types.str "My default"
+  ## ```
+  ##
+  #@ Type -> Any -> String
+  mkOpt' = type: default: mkOpt type default null;
+
+  ## Create a boolean NixOS module option.
+  ##
+  ## ```nix
+  ## lib.mkBoolOpt true "Description of my option."
+  ## ```
+  ##
+  #@ Type -> Any -> String
+  mkBoolOpt = mkOpt types.bool;
+
+  ## Create a boolean NixOS module option without a description.
+  ##
+  ## ```nix
+  ## lib.mkBoolOpt true
+  ## ```
+  ##
+  #@ Type -> Any -> String
+  mkBoolOpt' = mkOpt' types.bool;
+
+  tailnet = "fluffy-rooster.ts.net";
+  # font-mono = " Iosevka Nerd Font Mono";
+  # font = "Iosevka Nerd Font Propo";
+
+  # font = "FiraCode Nerd Font";
+  font = "MononokiNerdFont";
+
+  flakeRoot = self.outPath;
+  font-mono = "${font}Mono";
+  font-propo = "${font}Propo";
+
+  # font-mono = "Agave Nerd Font Mono";
+  # font = "Agave Nerd Font";
+
+  enabled = {
+
+    ## Quickly enable an option.
+    ##
+    ## ```nix
+    ## services.nginx = enabled;
+    ## ```
+    ##
+    #@ true
+    enable = true;
+  };
+
+  disabled = {
+    ## Quickly disable an option.
+    ##
+    ## ```nix
+    ## services.nginx = enabled;
+    ## ```
+    ##
+    #@ false
+    enable = false;
+  };
+}
