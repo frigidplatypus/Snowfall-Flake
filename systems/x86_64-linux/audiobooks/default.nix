23aee9c84778687c73aaf43c9338a6403ec1549d
Justin Martin <1242435+frigidplatypus@users.noreply.github.com>
2025-11-12 21:09:47 -0600

    update tsidp

--- systems/x86_64-linux/audiobooks/default.nix
+++ systems/x86_64-linux/audiobooks/default.nix
@@ -12,21 +12,21 @@ with lib.frgd;
     virtualHosts = {
       "audiobooks.fluffy-rooster.ts.net" = {
         extraConfig = ''
-          forward_auth unix//run/tailscale-nginx-auth/tailscale-nginx-auth.sock {
-            uri /auth
-          	header_up Remote-Addr {remote_host}
-          	header_up Remote-Port {remote_port}
-          	header_up Original-URI {uri}
-            	copy_headers {
-                Tailscale-User>X-Webauth-User
-                Tailscale-Name>X-Webauth-Name
-                Tailscale-Login>X-Webauth-Login
-                Tailscale-Tailnet>X-Webauth-Tailnet
-                Tailscale-Profile-Picture>X-Webauth-Profile-Picture
-              }
-
-          }
-          reverse_proxy localhost:8000
+          #   forward_auth unix//run/tailscale-nginx-auth/tailscale-nginx-auth.sock {
+          #     uri /auth
+          #   	header_up Remote-Addr {remote_host}
+          #   	header_up Remote-Port {remote_port}
+          #   	header_up Original-URI {uri}
+          #     	copy_headers {
+          #         Tailscale-User>X-Webauth-User
+          #         Tailscale-Name>X-Webauth-Name
+          #         Tailscale-Login>X-Webauth-Login
+          #         Tailscale-Tailnet>X-Webauth-Tailnet
+          #         Tailscale-Profile-Picture>X-Webauth-Profile-Picture
+          #       }
+          #
+          #   }
+            reverse_proxy localhost:8000
         '';
       };
     };
