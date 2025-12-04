0ac56ba0ef37f2613efd64e3dedbfd6ef0a801ae
Justin Martin <jus10mar10@gmail.com>
2025-12-03 23:42:38 -0600

    update tlp config for better performance on battery

--- modules/nixos/services/tlp/default.nix
+++ modules/nixos/services/tlp/default.nix
@@ -23,15 +23,15 @@ in
         CPU_SCALING_GOVERNOR_ON_AC = "performance";
         CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+        CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
 
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 50;
+        CPU_MAX_PERF_ON_BAT = 75;
 
-        CPU_BOOST_ON_BAT = 0;
+        CPU_BOOST_ON_BAT = 1;
         START_CHARGE_THRESH_BAT0 = 50;
         STOP_CHARGE_THRESH_BAT0 = 90;
         RUNTIME_PM_ON_BAT = "auto";
