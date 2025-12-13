867755cc3c498f5799136c55fd3d009b39e7dbfa
Justin Martin <jus10mar10@gmail.com>
2025-12-12 22:41:05 -0600

    add t480 key to ssh

--- modules/nixos/services/openssh/default.nix
+++ modules/nixos/services/openssh/default.nix
@@ -24,6 +24,7 @@ let
   probook-key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDIj24tr9NcCrRVFSj1Nk2DSx3gFWHxAWIV36EBh6UDWFtZLtTCDYTDvk/z0okL3Dkhjf2VIIkHuWTCHz7KGKJcxvM+pjdxFl/rLocMOkGn2uIWRbgk+yHnc62ARosy/VQ1UcHswTman8KdlSRU2qjuK3etP26JsKsUhd+/qXr+HSTlcJFrZH+YOVar/K3HklCxOrSXnNSLmJEvbC7s12EgQYiP1J2KLP18LsXmB6aNwpvendeooDZpDayrJ7qxuYJhSjzHTmLoNBxxDe1IuFlH4HdwtnrNI++v4tzfKNA3FnQ6r1CRLtNjrkIjDylI8GrfMr8rLDCEcUUzsXqnuhiMLmQwFNVCfFCmU02G60uuEpZlwxGGrZRCT6MQWu9NRlwIpZ9b/DvAmcBj71CuBtAoZSulVQ1KytvV+vuTp80s04n+YQJTxDLvPoJMXUJ+sTQieib8IfOzwVCNmx98pBvxr71XGEwpPWX2SViD8DYfzND8NLo4eq0WF2eXFBY3p3U= justin@nixos";
   nixserver-key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCrOrtvn3cWVvqHsF5ueQ4Qqod0hDE2jLagGosZqbH70H3aUOYq0ZeZ3EuwldAPMYATjz7azO6N7pVp0u4XJtJr5+yxgrBFmcr5eXilgR/8RHTf93BO+kZpkUOFFBNoAis0kQZ1dba/BFznsZDnvuie1G1evsJjAyQWhAizeDRW1MmjVQ/GfXLI8OY6mUZ2/W4CIItcq64pS0N0A5nnYmrd96Y/VOLCtf57nW6tcevN87vJpXrGV0iHnuax47OAUGFCibdRQg5eiyrf0r36jtPhOF5tJwP98JDlY0qR8Fp/pJaclZM6xY6fPA6lAxyfnTmMLpywsBQ1gSPm1/boTpLLGryniHZ3borcLnzqyJ+f47U/uMz9ILBkpV2L71KCXySng3yF//Tuf1hS//SdQwbpOKTgAZOkBcbdu6hHNLfkBkcoHND+zclVPueHJeP2+icRKNHPiR4oakRDjiP3x9MaocmsBSdY5AG5JmcZbd9/SFIOtmF4jAhhR2iRf2qkeWk= justin@nix";
   p5810-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFaf3lNuwDvzTl5wIe99h3PfwwyaNRiiGI8rHeFcxCwg justin@tsnix";
+  t480-key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDq8dTBM/d3El1DUy/MThcM2Tg/AyROHGECGCGdEcTOr justin@t480";
 
   other-hosts = lib.filterAttrs (
     key: host: key != name && (host.config.frgd.user.name or null) != null
@@ -59,6 +60,7 @@ in
       probook-key
       nixserver-key
       p5810-key
+      t480-key
     ] "The public keys to apply.";
     #port = mkOpt port 2222 "The port to listen on (in addition to 22).";
     manage-other-hosts =
