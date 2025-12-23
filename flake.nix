9f198ceaae6c4bdc7f255183f7b5aaf5a3e811d6
Justin Martin <jus10mar10@gmail.com>
2025-12-22 20:13:25 -0600

    cleanup insecure packages

--- flake.nix
+++ flake.nix
@@ -132,8 +132,8 @@
       channels-config = {
         allowUnfree = true;
         permittedInsecurePackages = [
-          "ventoy-1.1.05"
-          "libsoup-2.74.3"
+          # "ventoy-1.1.05"
+          # "libsoup-2.74.3"
           # "electron-27.3.11"
           # "electron-28.3.3"
           # "olm-3.2.16"
