5624140ff3a8a26ff4b53ff3da66fc9a39e618a2
Justin Martin <jus10mar10@gmail.com>
2026-02-17 15:47:18 -0600

    remove static ip for klipper, access is via tailscale regardless

--- systems/x86_64-linux/klipper/default.nix
+++ systems/x86_64-linux/klipper/default.nix
@@ -10,14 +10,6 @@ with lib.frgd;
   # Enable networking
   networking = {
     networkmanager.enable = true;
-    interfaces.enp4s0.ipv4.addresses = [
-      {
-        address = "192.168.0.8";
-        prefixLength = 24;
-      }
-    ];
-    defaultGateway = "192.168.0.1";
-    nameservers = [ "192.168.0.1" ];
   };
   networking.firewall.enable = false;
   frgd = {
