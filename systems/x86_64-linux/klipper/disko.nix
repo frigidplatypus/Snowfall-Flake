574fe7bd907a2afe83f5b4a04d9a8023742e5a55
Justin Martin <jus10mar10@gmail.com>
2026-02-18 21:08:03 -0600

    update disko configs

--- systems/x86_64-linux/klipper/disko.nix
+++ systems/x86_64-linux/klipper/disko.nix
@@ -14,6 +14,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot/efi";
+                mountOptions = [ "umask=0077" ];
               };
             };
             zfs = {
