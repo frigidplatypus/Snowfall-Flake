643b003867de353b93d55633e8609bf7120e1095
Justin Martin <jus10mar10@gmail.com>
2026-03-13 21:08:38 -0500

    home/services: add nix-index module

---
+++ modules/home/services/nix-index/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+
+with lib;
+with lib.frgd;
+let
+  cfg = config.frgd.services.nix-index;
+in
+{
+  options.frgd.services.nix-index = with types; {
+    enable = mkBoolOpt false "Whether or not to enable nix-index.";
+  };
+
+  config = mkIf cfg.enable {
+    programs.nix-index = {
+      enable = true;
+      enableFishIntegration = true;
+      enableNushIntegration = true;
+      enableBashIntegration = true;
+    };
+  };
+}
