caa43d22d0cfe9caeed3c9948c537dcec18e98b3
Justin Martin <jus10mar10@gmail.com>
2026-03-24 16:23:30 -0500

    openbox config

--- homes/x86_64-linux/justin@logseq/default.nix
+++ homes/x86_64-linux/justin@logseq/default.nix
@@ -1,4 +1,4 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 with lib;
 with lib.frgd;
 {
@@ -31,4 +31,39 @@ with lib.frgd;
       charms = enabled;
     };
   };
+
+  programs.git = {
+    enable = true;
+    settings = {
+      user = {
+        name = "Justin Martin";
+        email = "jus10mar10@gmail.com";
+      };
+      init = {
+        defaultBranch = "main";
+      };
+      pull = {
+        rebase = true;
+      };
+      push = {
+        autoSetupRemote = true;
+      };
+    };
+  };
+
+  xdg.configFile."openbox/menu.xml".text = ''
+    <?xml version="1.0" encoding="UTF-8"?>
+    <openbox_menu xmlns="http://openbox.org/3.4/menu">
+      <menu id="root-menu" label="Menu">
+        <item label="Terminal"><action name="Execute"><command>xterm</command></action></item>
+        <item label="Logseq"><action name="Execute"><command>logseq</command></action></item>
+        <item label="Obsidian"><action name="Execute"><command>obsidian</command></action></item>
+        <separator />
+        <item label="Reboot"><action name="Execute"><command>systemctl reboot</command></action></item>
+        <item label="Shutdown"><action name="Execute"><command>systemctl poweroff</command></action></item>
+        <separator />
+        <item label="Logout"><action name="Execute"><command>openbox --exit</command></action></item>
+      </menu>
+    </openbox_menu>
+  '';
 }
