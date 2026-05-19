a54a605e812b2e42ec7635e327d9a8134098ce24
Justin Martin <jus10mar10@gmail.com>
2026-05-19 14:01:22 -0500

    feat: add sb and silverbullet packages

---
+++ packages/sb/default.nix
@@ -0,0 +1,70 @@
+{
+  autoPatchelfHook,
+  common-updater-scripts,
+  fetchzip,
+  lib,
+  stdenv,
+  stdenvNoCC,
+  writeShellScript,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "sb";
+  version = "2.8.0";
+
+  src =
+    finalAttrs.passthru.sources.${stdenv.hostPlatform.system}
+      or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+
+  dontUnpack = true;
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp $src/sb $out/bin/
+    runHook postInstall
+  '';
+
+  passthru = {
+    sources = {
+      "x86_64-linux" = fetchzip {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/sb-linux-x86_64.zip";
+        hash = "sha256-dXA14flNLT88rbLHulnVvzCUyqcPlnQwYJxiMrNpJy4=";
+        stripRoot = false;
+      };
+      "aarch64-linux" = fetchzip {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/sb-linux-aarch64.zip";
+        hash = "sha256-8+MpL8Ob2VQDwjIrOcjWCIjEnsZAW5Zj5bfZSVjAl2k=";
+        stripRoot = false;
+      };
+      "x86_64-darwin" = fetchzip {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/sb-darwin-x86_64.zip";
+        hash = "sha256-c9PrV144B23UaAI2vWJfn6yF+MpY4j9NrssCr81yOy0=";
+        stripRoot = false;
+      };
+      "aarch64-darwin" = fetchzip {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/sb-darwin-aarch64.zip";
+        hash = "sha256-QO2yQoOhWZrXN+QVwi6MR6oKVK9V4BXiqIMW2qPJqu8=";
+        stripRoot = false;
+      };
+    };
+
+    updateScript = writeShellScript "update-sb" ''
+      NEW_VERSION="$1"
+      for platform in ${lib.escapeShellArgs finalAttrs.meta.platforms}; do
+        ${lib.getExe' common-updater-scripts "update-source-version"} "sb" "$NEW_VERSION" --ignore-same-version --source-key="sources.$platform"
+      done
+    '';
+  };
+
+  meta = {
+    changelog = "https://github.com/silverbulletmd/silverbullet/blob/${finalAttrs.version}/website/CHANGELOG.md";
+    description = "CLI client for SilverBullet";
+    homepage = "https://silverbullet.md";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ aorith ];
+    mainProgram = "sb";
+    platforms = builtins.attrNames finalAttrs.passthru.sources;
+  };
+})
---
+++ packages/silverbullet/default.nix
@@ -0,0 +1,75 @@
+{
+  autoPatchelfHook,
+  common-updater-scripts,
+  fetchzip,
+  lib,
+  nixosTests,
+  stdenv,
+  stdenvNoCC,
+  writeShellScript,
+}:
+stdenvNoCC.mkDerivation (finalAttrs: {
+  pname = "silverbullet";
+  version = "2.8.0";
+
+  src =
+    finalAttrs.passthru.sources.${stdenv.hostPlatform.system}
+      or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+
+  nativeBuildInputs = lib.optionals stdenv.hostPlatform.isLinux [ autoPatchelfHook ];
+
+  buildInputs = [ stdenv.cc.cc.lib ];
+
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    cp $src/silverbullet $out/bin/
+    runHook postInstall
+  '';
+
+  passthru = {
+    sources = {
+      "x86_64-linux" = fetchzip {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-linux-x86_64.zip";
+        hash = "sha256-qfUJnIOj3cRsjt9evdAUQc3J2fKwfrD6ST9zIUryj6U=";
+        stripRoot = false;
+      };
+      "aarch64-linux" = fetchzip {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-linux-aarch64.zip";
+        hash = "sha256-bU2xzHe041fzNvorYdHjRiVmCQQAxrZU/NEjWvxh3cA=";
+        stripRoot = false;
+      };
+      "x86_64-darwin" = fetchzip {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-darwin-x86_64.zip";
+        hash = "sha256-F0kIj0LxB0NnPzXJKK5UAQ1ARWT2KW/PkugELnJRc34=";
+        stripRoot = false;
+      };
+      "aarch64-darwin" = fetchzip {
+        url = "https://github.com/silverbulletmd/silverbullet/releases/download/${finalAttrs.version}/silverbullet-server-darwin-aarch64.zip";
+        hash = "sha256-p9Go2Be65/4uhxae1y1mmsuettiMzEBTm8uV49hrUzg=";
+        stripRoot = false;
+      };
+    };
+
+    updateScript = writeShellScript "update-silverbullet" ''
+      NEW_VERSION="$1"
+      for platform in ${lib.escapeShellArgs finalAttrs.meta.platforms}; do
+        ${lib.getExe' common-updater-scripts "update-source-version"} "silverbullet" "$NEW_VERSION" --ignore-same-version --source-key="sources.$platform"
+      done
+    '';
+
+    tests = {
+      inherit (nixosTests) silverbullet;
+    };
+  };
+
+  meta = {
+    changelog = "https://github.com/silverbulletmd/silverbullet/blob/${finalAttrs.version}/website/CHANGELOG.md";
+    description = "Open-source, self-hosted, offline-capable Personal Knowledge Management (PKM) web application";
+    homepage = "https://silverbullet.md";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ aorith ];
+    mainProgram = "silverbullet";
+    platforms = builtins.attrNames finalAttrs.passthru.sources;
+  };
+})
