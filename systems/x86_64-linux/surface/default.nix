fab1fb05884c2aabc7f7c1e85b17aa1138b47435
Justin Martin <jus10mar10@gmail.com>
2026-09-22 08:10:42 -0500

    surface: substitute surface kernel from frgd-surface-kernel Cachix

--- systems/x86_64-linux/surface/default.nix
+++ systems/x86_64-linux/surface/default.nix
@@ -80,6 +80,11 @@ with lib.frgd;
     nix = {
       enable = true;
       github-access-token = enabled;
+      extra-substituters = {
+        "https://frgd-surface-kernel.cachix.org" = {
+          key = "frgd-surface-kernel.cachix.org-1:pY13n7rTq2oCbPfrE3c45+2Uqfjo9tCQ+eY3NC9k0vo=";
+        };
+      };
     };
     system = {
       boot = {
